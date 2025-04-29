e15d20390f4f1abd5697c545448c6098805e2183
Joffrey Trebot <trebotjoffrey@gmail.com>
2025-04-29 23:59:14 +0200

    Initial Commit

---
+++ Sonea/SoneaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoneaApp.swift
+//  Sonea
+//
+//  Created by Joffrey Trébot on 4/29/25.
+//
+
+import SwiftUI
+
+@main
+struct SoneaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SoneaTests/SoneaTests.swift
@@ -0,0 +1,17 @@
+//
+//  SoneaTests.swift
+//  SoneaTests
+//
+//  Created by Joffrey Trébot on 4/29/25.
+//
+
+import Testing
+@testable import Sonea
+
+struct SoneaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
